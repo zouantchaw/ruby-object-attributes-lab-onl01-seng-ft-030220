f0ef8ca3186ee8ab29dfed1e5c52bb0cbd1746be
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-16 00:24:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  #setter name
+  def name=(dog_name)
+    @name = dog_name
+  end
+  #getter name
+  def name
+    @name
+  end
+
+  #setter breed
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+  #getter breed
+  def breed
+    @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  #setter name
+  def name=(person_name)
+    @name = person_name
+  end
+  #getter name
+  def name
+    @name
+  end
+
+  #setter job
+  def job=(job_title)
+    @job = job_title
+  end
+  #getter job
+  def job
+    @job
+  end
+end
